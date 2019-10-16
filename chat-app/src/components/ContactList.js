4be352f72f3d4befff1ad38989bca711d2853e83
Cyril Carral <cyril.carral85@gmail.com>
2019-10-16 17:48:58 +0200

    list done

---
+++ chat-app/src/components/ContactList.js
@@ -0,0 +1,40 @@
+import React from "react";
+import Contact from "./Contact";
+
+const users = [
+  {
+    name:"Felecia Ryan",
+    avatar:"https://randomuser.me/api/portraits/women/59.jpg",
+    online:true
+  },
+  {
+    name:"Mia Hill",
+    avatar:"https://randomuser.me/api/portraits/women/44.jpg",
+    online:true
+  },
+  {
+    name:"Jared Bryant",
+    avatar:"https://randomuser.me/api/portraits/men/21.jpg",
+    online:false
+  },
+  {
+    name:"Shane Collins",
+    avatar:"https://randomuser.me/api/portraits/men/6.jpg",
+    online:false
+  },
+  {
+    name:"Nina Cunningham",
+    avatar:"https://randomuser.me/api/portraits/women/96.jpg",
+    online:true
+  }
+];
+
+const ContactList = () => (
+  <div>
+    {users.map(item => (
+  <Contact name={item.name} image={item.avatar} online={item.online}/>
+))}
+  </div>
+);
+
+export default ContactList;
